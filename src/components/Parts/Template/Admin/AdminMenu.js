e74435912a4a29f86a41d79fcf8df1bb357be971
naotarian <naotarian19@gmail.com>
2022-12-13 00:22:13 +0900

    admin menu追加

---
+++ src/components/Parts/Template/Admin/AdminMenu.js
@@ -0,0 +1,188 @@
+import { useState } from 'react'
+import { styled, useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import CssBaseline from '@mui/material/CssBaseline'
+import MuiAppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+const drawerWidth = 240
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}))
+
+const AdminMenu = props => {
+  const theme = useTheme()
+  const { open, setOpen } = props
+  const [expanded, setExpanded] = useState([])
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
+    setExpanded(
+      isExpanded
+        ? [...expanded, panel]
+        : expanded.filter((fruit, index) => fruit !== panel),
+    )
+  }
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            店名様
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List style={{ padding: 0 }}>
+          {menus.map((text, index) => {
+            return (
+              <Accordion
+                expanded={expanded.includes('panel' + index)}
+                onChange={handleChange('panel' + index)}
+                style={{ margin: 0, padding: 0, boxShadow: 'none' }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  id="panel1bh-header"
+                  style={{
+                    paddingRight: '1rem',
+                    paddingLeft: 0,
+                    maxHeight: '40px',
+                  }}>
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text.name} />
+                    </ListItemButton>
+                  </ListItem>
+                </AccordionSummary>
+                <AccordionDetails style={{ padding: 0 }}>
+                  <List>
+                    {text.sub.map((text, index) => (
+                      <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                          <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                          </ListItemIcon>
+                          <ListItemText primary={text.name} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                </AccordionDetails>
+              </Accordion>
+            )
+          })}
+        </List>
+        <Divider />
+      </Drawer>
+    </Box>
+  )
+}
+
+export default AdminMenu
+
+const menus = [
+  {
+    name: '基本情報',
+    sub: [{ name: '営業情報', path: '/admin/dashbord/salesInformation' }],
+  },
+  {
+    name: 'スタッフ管理',
+    sub: [
+      { name: 'スタッフ登録', path: '/admin/dashbord/salesInformation' },
+      { name: 'シフト登録', path: '/admin/dashbord/salesInformation' },
+    ],
+  },
+  {
+    name: '予約管理',
+    sub: [{ name: '予約状況', path: '/admin/dashbord/salesInformation' }],
+  },
+  {
+    name: '施設管理',
+    sub: [
+      { name: '座席管理', path: '/admin/dashbord/salesInformation' },
+      { name: '座席予約状況', path: '/admin/dashbord/salesInformation' },
+    ],
+  },
+]
--- src/pages/admin/dashbord/index.js
+++ src/pages/admin/dashbord/index.js
@@ -1,9 +1,18 @@
 import axios from '@/lib/axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Header from '@/components/Parts/Template/Admin/Header'
+import AdminMenu from '@/components/Parts/Template/Admin/AdminMenu'
+import { styled, useTheme } from '@mui/material/styles'
+import Grid from '@mui/material/Grid'
+const WrapperGrid = styled(Grid)`
+  margin-left: ${props => (props.open ? '240px' : 0)};
+  padding: 1rem;
+`
 const index = () => {
   const router = useRouter()
+
+  const [open, setOpen] = useState(true)
   useEffect(() => {
     ;(async () => {
       try {
@@ -15,8 +24,8 @@ const index = () => {
   }, [])
   return (
     <>
-      <Header />
-      dashbord
+      <AdminMenu open={open} setOpen={setOpen} />
+      <WrapperGrid open={open}>dddd</WrapperGrid>
     </>
   )
 }
